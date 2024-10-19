2d13cbe8f7adf3a23ece49deb39fbbd273640642
H8K56 <drty_hry@protonmail.com>
2024-10-19 12:21:44 +0300

    basic routing for standard customer infomation

--- routes/bocRoutes.js
+++ routes/bocRoutes.js
@@ -38,6 +38,40 @@ router.get("/balance/:accountId", async (req,res) => {
     }
 })
 
+router.get("/statements/:accountId", async (req,res,date_params) => {
+    try{
+        const accountId = req.params.accountId;
 
+        const data = await BOC_API.getUserBankStatement({
+            accountId,
+            authorization: req.headers.authorization, // Pass token from headers
+            journeyId: "3802988197969920", 
+            timeStamp: "554370977", // The time stamp when the request was sent to system
+            subscriptionId: "31.204.105.234", //Subscription ID of a subscriber
+        },date_params.start_date,date_params.end_date);
+
+        res.json(data)
+    }catch (error){
+        res.status(500).json({error: error.message});
+    }
+})
+
+router.get("/balance/:accountId", async(req,res) => {
+    try {
+        const accountId = req.params.accountId;
+
+        const data = await BOC_API.getUserBankStatement({
+            accountId,
+            authorization: req.headers.authorization, // Pass token from headers
+            journeyId: "3802988197969920", 
+            timeStamp: "554370977", // The time stamp when the request was sent to system
+            subscriptionId: "31.204.105.234", //Subscription ID of a subscriber
+        })
+
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+})
 
 module.exports = router;
