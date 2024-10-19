107b444c1b8fb5c95a6412dedf24a54a92a4540f
H8K56 <drty_hry@protonmail.com>
2024-10-19 11:58:05 +0300

    frontend routing for balance and account details

---
+++ routes/bocRoutes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const BOC_API = require("../controllers/BOC_API");
+
+router.get("/account/:accountId", async (req, res) => {
+  try {
+    const accountId = req.params.accountId;
+
+    const data = await BOC_API.getUserAccountDetails({
+      accountId,
+      authorization: req.headers.authorization, // Pass token from headers
+      journeyId: "3802988197969920", 
+      timeStamp: "554370977", // The time stamp when the request was sent to system
+      subscriptionId: "31.204.105.234", //Subscription ID of a subscriber
+    });
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/balance/:accountId", async (req,res) => {
+    try{
+        const accountId = req.params.accountId;
+
+        const data = await BOC_API.getAccountBalance({
+            accountId,
+            authorization: req.headers.authorization, // Pass token from headers
+            journeyId: "3802988197969920", 
+            timeStamp: "554370977", // The time stamp when the request was sent to system
+            subscriptionId: "31.204.105.234", //Subscription ID of a subscriber
+        })
+
+        res.json(data)
+    }catch (error){
+        res.status(500).json({error: error.message});
+    }
+})
+
+
+
+module.exports = router;
